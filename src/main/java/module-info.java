193c82c57d3e23ede2457f59b813969249b0bd86
lucialiceri <63566999+lucialiceri@users.noreply.github.com>
2021-07-29 17:26:49 -0300

    lib move to proyect

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,7 @@
-/**
- *
- */
 module edu.fiuba.algo3 {
-    requires javafx.controls;
     exports edu.fiuba.algo3;
     requires javafx.fxml;
     exports edu.fiuba.algo3.view;
-    requires javafx.base;
-    requires javafx.graphics;
-    requires javafx.media;
+    requires javafx.controls;
     requires javafx.swing;
 }
