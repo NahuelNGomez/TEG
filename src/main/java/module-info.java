33188da0114065d38efcea876d2d8580239ea72b
CammSt <56458193+CammSt@users.noreply.github.com>
2021-08-01 17:07:15 -0300

    adding javafx to branch

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module edu.fiuba.algo3 {
-    requires javafx.controls;
     exports edu.fiuba.algo3;
+    requires javafx.fxml;
+    exports edu.fiuba.algo3.view;
+    requires javafx.controls;
+    requires javafx.swing;
 }
